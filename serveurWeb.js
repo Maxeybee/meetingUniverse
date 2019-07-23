aaa0bfd02be955a1dedf4608b71d8d18d32e681f
maxeybee <etienne.ludot@outlook.fr>
2019-07-23 22:40:46 +0200

    modif rien dutout

--- serveurWeb.js
+++ serveurWeb.js
@@ -1,9 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
-  res.end("Hello World");  // faire la redirection vers l'index ici retirer le hello world
+  res.end('Hello moto');  // faire la redirection vers l'index ici retirer le hello world
 });
-
 server.listen(1337);
 console.log("Serveur web lancé sur localhost:1337 ...");
-
