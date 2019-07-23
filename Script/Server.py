a4f578589c5163b9174129e2fc160fc2b2d8b43d
azrael2162 <vincent.pinelli2162@gmail.com>
2019-07-23 23:56:11 +0200

    Delete Server.py

--- Script/Server.py
+++
@@ -1,9 +0,0 @@
-import argparse
-import os 
-parser = argparse.ArgumentParser()
-parser.add_argument("-start", help="Start server")
-parser.add_argument("-stop", help="Stop server")
-
-args = parser.parse_args()
-if args.start:
-    os.system("node serveurWeb.js")
