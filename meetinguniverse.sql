f0ae16d8cb7d6eec5c6b766f9f43ca3485b43722
Etienne <36929336+Maxeybee@users.noreply.github.com>
2019-07-24 10:47:37 +0200

    update bdd

--- meetinguniverse.sql
+++ meetinguniverse.sql
@@ -1,11 +1,11 @@
-
 DROP TABLE IF EXISTS project;
 CREATE TABLE IF NOT EXISTS project (
   id_project int(11) NOT NULL AUTO_INCREMENT,
   id_owner int(11) NOT NULL,
   project_title varchar(25) NOT NULL,
   project_description text NOT NULL,
-  PRIMARY KEY (id_project)
+  PRIMARY KEY (id_project),
+  CONSTRAINT fk_id_user FOREIGN KEY (id_owner) REFERENCES user(id_user)
 );
 
 DROP TABLE IF EXISTS user;
