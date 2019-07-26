5f908ed3591177eea9ce2dde2b6c37ce06ca4cf7
VincentP1993 <maiden2164@gmail.com>
2019-07-26 13:49:09 +0200

    modif du routeur

--- serveurWeb.js
+++ serveurWeb.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var app = express();
 var port = 1337;
+var pages = [ 'about', 'connexion','deconnexion','test']; //tableau des pages
 
-// route vers l'index
+
+
+// la route pour afficher l'index
 app.get("/", function(req, res){
   res.sendFile(__dirname +'/index.html')
+
 });
 
-app.listen(port, function(){
-  console.log("Serveur web lancé sur localhost:1337")
+
+//les vues sont dans le dossier view/
+//la route pour afficher les pages$
+pages.forEach(function(unePage){
+  app.get("/"+unePage+"/", function(req, res){
+    res.sendFile(__dirname +'/view/'+unePage+'.html')
+  })
 });
+
+
+
+
+
+
+//Ecoute sur le Port 1337
+app.listen(port, console.log("Serveur web lancé sur localhost:1337"));
